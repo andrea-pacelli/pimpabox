0c5cb550b90f307b0f54b2bf9b8f449df4d3fbf4
Andrea Pacelli <ap@ieee.org>
2024-12-14 17:41:26 -0500

    Replaced IMU-based song selection with EEPROM-based song rotation.

--- pimpabox.ino
+++ pimpabox.ino
@@ -11,11 +11,19 @@
 #include <SPI.h>
 #include <AudioZero.h>
 #include <Arduino_LSM6DS3.h>
+#include <ArduinoECCX08.h>
 
 constexpr int pin_battery = A1;
 constexpr int pin_power = 2;
 constexpr int pin_shutdown = 3;
 constexpr int pin_SD_CS = 10;
+
+struct State {
+  int magic = 42;
+  int last_song = 1;
+};
+
+State state;
 
 void playFile(char *filename) {
   Serial.print("Playing ");
@@ -39,10 +47,19 @@ void setup(void) {
   // Shutdown amp
   pinMode(pin_shutdown, OUTPUT);
   digitalWrite(pin_shutdown, HIGH);
+  // Measure battery voltage
+  analogReadResolution(10);
+  constexpr float r1 = 68;
+  constexpr float r2 = 33;
+  const float v_battery =
+    (analogRead(pin_battery) / 1024.0) * 3.3 * (r1 + r2) / r2;
+  constexpr float v_battery_low = 5.2;
   // Start Serial
   Serial.begin(9600);
   const unsigned long millis0 = millis();
-  while (!Serial && millis() - millis0 < 2000)
+  while (v_battery < v_battery_low
+	 && !Serial
+	 && millis() - millis0 < 5000)
     yield();
   Serial.println("*** PimpaBox ***");
   Serial.println("***  AP2024. ***");
@@ -59,42 +76,49 @@ void setup(void) {
     Serial.println("IMU begin error");
     while (true);
   }
-  // Check battery status
-  analogReadResolution(10);
-  constexpr float r1 = 68;
-  constexpr float r2 = 33;
-  const float v_battery =
-    (analogRead(pin_battery) / 1024.0) * 3.3 * (r1 + r2) / r2;
+  // Shutdown if battery low
   Serial.print("v_battery=");
   Serial.println(v_battery, 3);
-  if (v_battery < 5.2) {
+  if (v_battery < v_battery_low) {
     char filename[] = "sad.wav";
     playFile(filename);
     digitalWrite(pin_power, LOW);
   }
-  randomSeed(analogRead(pin_battery));
+  // Read state
+  State state_temp;
+  if (!ECCX08.begin()) {
+    Serial.println("ECCX08 begin error");
+    while (true);
+  }
+  if (!ECCX08.locked()) {
+    Serial.println("ECCX08 not locked");
+    while (true);
+  }
+  int ret =
+    ECCX08.readSlot(8, (byte*)&state_temp, sizeof(state_temp));
+  if (ret != 1) {
+    Serial.println("ECCX08 read error");
+    while (true);
+  }
+  if (state_temp.magic == state.magic) {
+    state = state_temp;
+  }
 }
 
 void loop(void) {
-  float a[3];
-  IMU.readAcceleration(a[0], a[1], a[2]);
-  Serial.print("a=(");
-  Serial.print(a[0]);
-  Serial.print(",");
-  Serial.print(a[1]);
-  Serial.print(",");
-  Serial.print(a[2]);
-  Serial.println(")");
-  int song_number = 0;
-  for (int i = 0; i < 3; i++) {
-    const int j = 1 + 2 * i;
-    if (a[i] > 0.5) {
-      song_number = j;
-    }
-    else if (a[i] < -0.5) {
-      song_number = j + 1;
-    }
+  // Next song
+  int song_number = state.last_song + 1;
+  if (song_number > 6) {
+    song_number = 2;
   }
+  state.last_song = song_number;
+  // Save song
+  int ret = ECCX08.writeSlot(8, (byte*)&state, sizeof(state));
+  if (ret != 1) {
+    Serial.println("ECCX08 write error");
+    while (true);
+  }
+  // Play song
   char filename[10];
   sprintf(filename, "%s%d.wav",
 	  (song_number < 10 ? "0" : ""),
